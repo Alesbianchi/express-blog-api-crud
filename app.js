cfc068fbb0bd6ac7b79291bc4d44074a243db06f
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-02-10 15:14:51 +0100

    add function and change file name

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+
 // importo il file delle rotte
 const postsRouter = require('./router/posts');
+
+// importo il middleware
+const errorHandlers = require('./middlewares/errorHandlers');
+
 
 
 //definisco la cartella per i file statici
 app.use(express.static('pubblic'));
+
 
 
 //registro il body parser
@@ -19,8 +25,14 @@ app.get('/', (req, res) => {
     res.send("Server del mio blog");
 })
 
+
 //utilizziamo la rotta dei blog andando a definire la parte iniziale delle rotte
 app.use("/posts", postsRouter);
+
+//autorizzo il middleware
+app.use(errorHandlers);
+
+
 
 //avvio del server sulla porta indicata
 app.listen(port, () => {
